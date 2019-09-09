794d055397ae1fca87361e66bac0ea651ad3208c
juliancamilocamchotorres <54864803+juliancamilocamchotorres@users.noreply.github.com>
2019-09-08 19:44:45 -0500

    Add files via upload
    Simple and compound conditional structures

---
+++ example-if-1.py
@@ -0,0 +1,18 @@
+num1=int(input("ingrese el primer valor: "))
+num2=int(input("ingrese el segundo valor: "))
+if num1>num2:
+    resultado=num1+num2
+    print("La suma es: ")
+    print(resultado)
+    resultado=num1-num2
+    print("La resta es: ")
+    print(resultado)  
+
+else:
+    resultado=num1*num2
+    print("El producto es: ")
+    print(resultado)
+    resultado=num2/num1
+    print("El division es: ")
+    print(resultado)
+
